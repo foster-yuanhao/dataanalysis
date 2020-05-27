957f2fd71a72d5e950dc9164d4d00563b92cf1a9
foster_lee <47941639+foster-yuanhao@users.noreply.github.com>
2020-05-27 17:31:44 +0800

    Update asg2.py

--- asg2.py
+++ asg2.py
@@ -7,7 +7,7 @@ This is a temporary script file.
 import numpy
 #declear the matrix for data
 array=[1100,1225,1359,1600,1750,1482,1700,\
-        900,1211,1203,1023,900,1322,\
+        1700,900,1211,1203,1023,900,1322,\
         1191,1280,1740,1360,1402,1342,1340,\
         676,567,1094,830,747,781,941,\
         1356,1289,1368,1147,1204,1301,1356,\
@@ -30,15 +30,6 @@ print(x)
     
 x = numpy.mean(array)
 print(x)
-    
-
-
-
-x = numpy.median(array)
-print(x)
-    
-
-
 
 import scipy
 from scipy import stats
